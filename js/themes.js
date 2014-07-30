263aff26d0c5e2d7e39860ad965b08119ceb677a
sconway <scoot1243@gmail.com>
2014-07-30 17:23:11 -0400

    responsive nav bar and full-width container

--- js/themes.js
+++ js/themes.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
   $('.carousel-nav li a').click(function(event) {
     var buttonNumber = $(this).attr("href").split('-').pop(),
         slideWidth   = $(".slide").width(),
-        slideAmount  = buttonNumber * slideWidth;
+        slideAmount  = buttonNumber * slideWidth * -1;
 
     $(".slider").animate({
-      left: "-=" + slideAmount
+      left: slideAmount
     });
   });
 });
