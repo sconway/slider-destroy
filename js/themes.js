169a04e81ef1fc999779e612f116c5b38507f044
sconway <scoot1243@gmail.com>
2014-07-31 12:03:40 -0400

    working with 3 elements. mostly working with more elements

--- js/themes.js
+++ js/themes.js
@@ -8,15 +8,19 @@ $(document).ready(function() {
   * widths of the slider and the slides at load time.
   */
   var setWidths = function() {
-    var numWindows      = numSlides/slidesPerWindow;
-    var newSliderWidth  = (100 * numWindows) + "%";
+    var numWindows           = numSlides/slidesPerWindow;
+    var newSliderWidthLarge  = (100 * numWindows) + "%";
+    var newSliderWidthSmall  = (100 * numSlides) + "%";
     var newSlideWidth   = (100 / numSlides)  + "%";
 
     if ($(window).width() > 400 ) {
-      alert($(window).width());
-      $(".slider").css("width", newSliderWidth);
+      $(".slider").css("width", newSliderWidthLarge);
+      $(".slide").css("width", newSlideWidth);
+    } else {
+      $(".slider").css("width", newSliderWidthSmall);
       $(".slide").css("width", newSlideWidth);
     }
+
   }
 
 
@@ -28,12 +32,14 @@ $(document).ready(function() {
     if (numSlides > slidesPerWindow) {
       $(".carousel-nav").css("display", "inherit");
     }
-
     $(".carousel-nav").css("width", numSlides*58);
   };
 
   displayNavButtons();
-  setWidths();
+
+  if (numSlides > 3){
+    setWidths();
+  }
 
   // slide the carousel, by a multiple of a slide's width, to the left
   $('.carousel-nav li a').click(function(event) {
